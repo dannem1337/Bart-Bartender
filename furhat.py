bdd54485db6d8a5729b18b8820d5ca5bdb34529b
Walter <walter.levens@gmail.com>
2024-11-29 14:27:49 +0100

    add todo's in file

--- furhat.py
+++ furhat.py
@@ -1,6 +1,12 @@
 from furhat_remote_api import FurhatRemoteAPI
 import time
 
+####### TODOs ######
+# make furhat follow a face (or multiple)
+# Connect furhat to LLM
+# Tune the LLM to be a bartender -> include a list of cocktails/ingredients to choose from
+####################
+
 furhat = FurhatRemoteAPI("localhost")
 furhat.set_voice(name='Matthew')
 
@@ -8,3 +14,5 @@ thread = furhat.attend(location="0.0,0.0,1.0", async_req=True)
 
 furhat.say(text="Whoa there!", async_req=True)
 furhat.gesture(name="Surprise")
+time.sleep(1)
+result = furhat.furhat_listen_get(async_req=True)
