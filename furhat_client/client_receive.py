bcd8d5cc4f0aaf39faa627497c8032dadc9f002c
Walter <walter.levens@gmail.com>
2024-12-03 15:41:23 +0100

    added integration with LLM

--- furhat_client/client_receive.py
+++ furhat_client/client_receive.py
@@ -3,11 +3,21 @@ import select
 import time
 import json
 from furhat_remote_api import FurhatRemoteAPI
+import google.generativeai as genai
 
 HOST = 'localhost'
 PORT = 65439
 
 ACK_TEXT = 'text_received'
+
+genai.configure(api_key="Your API key")
+model=genai.GenerativeModel(
+  model_name="gemini-1.5-flash",
+  system_instruction="""You are Bart the Bartender. It is your job to find out what 
+  cocktail fits the mood of the one that is talking to you. You do this by asking
+  considerate but short questions. Questions should be less than 2 sentences. After
+  3 questions you should suggest a cocktail that fits the mood of the user. Once
+  you suggest a cocktail, you should stop asking questions""")
 
 
 def main():
@@ -18,7 +28,6 @@ def main():
 
     # Set the voice of the robot
     furhat.set_voice(name='Matthew')
-    furhat.say(text="initiated")
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print('socket instantiated')
@@ -33,20 +42,24 @@ def main():
         except:
             pass
 
+    furhat.say(text="Hello, what can I do for you today?")
     socks = [sock]
     while True:
         readySocks, _, _ = select.select(socks, [], [], 5)
         for sock in readySocks:
             message = receiveTextViaSocket(sock)
-            result = furhat_recieve_text(furhat, message)
+            furhat_recieve_text(furhat, message)
             print('received: ' + str(message))
 
 
 def furhat_recieve_text(furhat, message):
     message = json.loads(message)
-    result = furhat.listen()
-    furhat.say(text=f"{message['no_faces']}", blocking=True)
-    return result
+    user_input = furhat.listen()
+    print("user message is:" + user_input.message)
+    ai_response = model.generate_content(user_input.message)
+    print(ai_response.text)
+    furhat.say(text=ai_response.text, blocking=True)
+    # furhat.say(text=f"{message['no_faces']}", blocking=True)
 
 
 def receiveTextViaSocket(sock):
--- furhat_client/furhat.py
+++ furhat_client/furhat.py
@@ -3,7 +3,13 @@ import time
 import google.generativeai as genai
 
 genai.configure(api_key="AIzaSyDVzqHc8FxpPiyGFXe9jkdiiSqa8U5iuQ0")
-model = genai.GenerativeModel("gemini-1.5-flash")
+model=genai.GenerativeModel(
+  model_name="gemini-1.5-flash",
+  system_instruction="""You are Bart the Bartender. It is your job to find out what 
+  cocktail fits the mood of the one that is talking to you. You do this by asking
+  considerate but short questions. Questions should be less than 2 sentences. After
+  3 questions you should suggest a cocktail that fits the mood of the user. Once
+  you suggest a cocktail, you should stop asking questions""")
 
 furhat = FurhatRemoteAPI("localhost")
 furhat.set_voice(name='Matthew')
@@ -12,16 +18,16 @@ furhat.attend(user="CLOSEST")
 
 
 # use something like this for the conversation
-furhat.say(text="Whoa there!", async_req=True)
+furhat.say(text="Hello, what can I do for you today?", async_req=True)
 furhat.gesture(name="Surprise")
 time.sleep(1)
 
 while True:
-    user_input_async = furhat.furhat_listen_get(async_req=True)
-    user_input = user_input_async.get()
-    ai_Response = model.generate_content(user_input)
-    furhat.say(text=ai_Response.text)
-    time.sleep(1)
+    user_input = furhat.listen()
+    print(user_input.message)
+    ai_response = model.generate_content(user_input.message)
+    print(ai_response.text)
+    furhat.say(text=ai_response.text, blocking=True)
     # Should call this function constantly. It just attends to the closest user in this
     # moment and keeps following that person, even though a different user comes closer later
     furhat.attend(user='CLOSEST')
